9fff4693321b9cb6741b26946b08de842ad389fb
S4g1tt4r1vs404D0XX <143964142+S4g1tt4r1vs404D0XX@users.noreply.github.com>
2024-08-31 19:15:59 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function selectLanguage(language) {
+    alert("You selected " + language + "!");
+    // Perform additional actions here, like redirecting to another page or loading specific content.
+    // For example:
+    // window.location.href = language.toLowerCase() + ".html";
+}
